cf5e03310a9d66a5a86bc0e5a11ddf25d16464c6
demeke <demeke.get23@gmail.com>
2022-12-03 01:28:41 +0300

    initial push

---
+++ src/app.module.ts
@@ -0,0 +1,31 @@
+import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { DataSource } from 'typeorm';
+import { UserEntity } from './entities/user.entity';
+import { PhotoEntity } from './entities/photo.entity';
+
+@Module({
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'localhost',
+      port: 5432,
+      username: 'postgres',
+      password: 'root',
+      database: 'orga_structure',
+      entities: [UserEntity, PhotoEntity],
+      synchronize: true,
+    }),
+  ],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule {
+  constructor(private dataSource: DataSource) {
+console.log(dataSource.toString())
+
+  }
+
+}
