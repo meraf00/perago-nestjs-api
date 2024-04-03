27fa5353f5a915569e33add52e6127afd2599849
Leul Wujira <meraf000@gmail.com>
2024-04-03 19:57:40 +0300

    feat: add role module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,6 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { DataSource } from 'typeorm';
-import { UserEntity } from './entities/user.entity';
-import { PhotoEntity } from './entities/photo.entity';
+import { RoleModule } from './role/role.module';
 
 @Module({
   imports: [
@@ -15,17 +11,11 @@ import { PhotoEntity } from './entities/photo.entity';
       username: 'postgres',
       password: 'root',
       database: 'orga_structure',
-      entities: [UserEntity, PhotoEntity],
+      entities: [__dirname + '/**/*.entity{.ts}'],
       synchronize: true,
     }),
+
+    RoleModule,
   ],
-  controllers: [AppController],
-  providers: [AppService],
 })
-export class AppModule {
-  constructor(private dataSource: DataSource) {
-console.log(dataSource.toString())
-
-  }
-
-}
+export class AppModule {}
